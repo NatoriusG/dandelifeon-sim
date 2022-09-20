3018e8f56683afea0743b9fd3e234825189ddd00
Nathaniel Edgar <nathaniel.edgar.fl@gmail.com>
2022-09-20 15:45:42 -0400

    initial typing: World, Cell, CellBoard alias

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
+const WIDTH: usize = 640;
+const HEIGHT: usize = 640;
+const BORDER: usize = 100;
+const WORLD_SIZE: usize = 25;
+const CELL_SIZE: usize = 64;
+
+struct Cell {
+    alive: bool,
+    age: usize,
+}
+
+type CellBoard = [[Cell; WORLD_SIZE]; WORLD_SIZE];
+
+struct World {
+    cells: CellBoard,
+}
+
 fn main() {}
